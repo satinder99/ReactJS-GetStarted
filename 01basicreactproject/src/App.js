6dea6c3332b22f8f2d5395f92d4290b938aecc65
Satinder Singh <kainth00013@gmail.com>
2023-10-30 03:00:01 -0400

    New App created using create-react-app

---
+++ 01basicreactproject/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <h1>Chai aur React | Satinder: Notes:
+      we have created a new project using the react create-react-app utility
+    </h1>
+  );
+}
+
+export default App;
