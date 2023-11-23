985341c0b605945e9797a015fb6ae611d7e11c75
Satinder Singh <kainth00013@gmail.com>
2023-11-23 03:48:08 -0500

    local storage used to store todo list

--- 10TodoContextLocal/src/App.jsx
+++ 10TodoContextLocal/src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react'
 import { TodoProvider } from './contexts'
+import { useEffect } from 'react'
+import TodoForm from './components/TodoForm'
+import TodoItem from './components/TodoItem'
 
 function App() {
   const [todos, setTodos] = useState([])
 
   const addTodo = (todo)=>{
-    setTodos((prev)=> [{id:Date.now(),...todo},...prev])
+    // console.log(todo)
+    setTodos((prev)=> [{...todo},...prev])
+    console.log(todos)
   }
 
   const updateTodo=(id, todo)=>{
@@ -26,6 +31,16 @@ function App() {
     )
   }
 
+  useEffect(()=>{
+    const todos = JSON.parse(localStorage.getItem("todos"))
+    if(todos && todos.length > 0)
+      setTodos(todos)
+  },[])
+
+  useEffect(()=>{
+    localStorage.setItem("todos",JSON.stringify(todos))
+  },[todos])
+
   return (
     <TodoProvider value={{todos, addTodo,updateTodo,deleteTodo,toggleComplete}}>
       <div className="bg-[#172842] min-h-screen py-8">
@@ -33,9 +48,17 @@ function App() {
               <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
               <div className="mb-4">
                   {/* Todo form goes here */} 
+                  <TodoForm/>
               </div>
               <div className="flex flex-wrap gap-y-3">
                   {/*Loop and Add TodoItem here */}
+                  {todos.map((todo)=>(
+                    <div key = {todo.id}
+                    className='w-full'>
+                      <TodoItem todo={todo}/>
+                    </div>
+                  ))
+                  }
               </div>
           </div>
       </div>
