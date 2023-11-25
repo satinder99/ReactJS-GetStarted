736edc9ece798613d0bdd3f3349b321b25bc2874
Satinder Singh <kainth00013@gmail.com>
2023-11-25 07:14:13 -0500

    project ready

---
+++ 11ReduxToolKitTodo/src/features/todo/todoSlice.js
@@ -0,0 +1,33 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit"
+
+const initialstate = {
+    todos:[
+        {
+            id:1,
+            text:"Hello Redux"
+        }
+    ]
+}
+
+
+export const todoSlice = createSlice({
+    name:'todo',
+    initialState : initialstate,
+    reducers:{
+        addTodo : (state, action)=>{
+            const todo = {
+                id:nanoid(),
+                text:action.payload
+            }
+            state.todos.push(todo);
+        },
+        removeTodo:(state,action)=>{
+            state.todos = state.todos.filter((todo)=> todo.id !== action.payload)
+        },
+    }
+})
+
+
+export const {addTodo, removeTodo} = todoSlice.actions
+
+export default todoSlice.reducer 
