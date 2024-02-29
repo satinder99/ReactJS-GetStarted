0ba4e1ba287407f58bfa1cd7bb9c28bc9225cad2
Satinder Singh <kainth00013@gmail.com>
2024-02-29 09:52:00 -0500

    components added in the functionality

--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -3,11 +3,18 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container"
 import Logo from "./Logo"
 import LogoutBtn from "./Header/LogoutBtn"
-
+import Select from "./Select"
+import PostCard from "./PostCard";
+import Input from "./Input";
+import Button from "./Button";
 export {
     Header,
     Footer,
     Container,
     Logo,
     LogoutBtn,
+    Select,
+    PostCard,
+    Input,
+    Button,
 }
